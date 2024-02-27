63ebf31137ad7ad4f17fe65b436251cbba986a7f
WriteRead024 <rich@rwhifonline.net>
2024-02-27 14:29:52 -0500

    b.m.t. WIP

--- SubfolderThree-BranchTests/branch-merge-test.py
+++ SubfolderThree-BranchTests/branch-merge-test.py
@@ -20,7 +20,7 @@ def print_test_info():
     print("1. (automatic) Create a new directory if it does not exist.")
     print("2. (automatic) Create a new Markdown file with some text in the directory.")
     print("3. (automatic) Commit the new file to the current branch.")
-    print("4. (manual) Create a new branch with a timestamp name.")
+    print("4. (automatic) Create a new branch with a timestamp name.")
     print("5. (manual) Make some changes to the file in the new branch.")
     print("6. (manual) Commit the changes to the new branch.")
     print("7. (manual) Merge the new branch into the main branch.")
@@ -61,12 +61,23 @@ def commit_file_to_git():
     print("File committed to git successfully.")
 
 
-def check_git_status():
-    subprocess.run(["git", "status"])
+def git_status_is_clean() -> bool:
+    result = subprocess.run(["git", "status"], capture_output=True, text=True)
+    output = result.stdout
+    if "nothing to commit, working tree clean" in output:
+        return True
+    else:
+        return False
 
 
 # Main function
 if __name__ == "__main__":
+    # check if git status is clean, and if not, exit
+    if not git_status_is_clean():
+        print("git status is not clean. Please commit or stash your changes.")
+        print("Exiting script.")
+        sys.exit()
+    # if directory name is provided proceed with test
     if len(sys.argv) < 2:
         print("Please provide a directory name as a command line argument.")
         print("Or input '--help' for more information.")
@@ -79,3 +90,4 @@ if __name__ == "__main__":
             create_directory_if_dne(directory_name)
             check_if_file_exists()
             create_new_Markdown_file()
+            commit_file_to_git()
