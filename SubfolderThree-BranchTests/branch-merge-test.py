9ce5e060527941542dc189aa09b7cc2b1b3d8c62
WriteRead024 <rich@rwhifonline.net>
2024-02-27 14:48:08 -0500

    more branch merge test work

--- SubfolderThree-BranchTests/branch-merge-test.py
+++ SubfolderThree-BranchTests/branch-merge-test.py
@@ -7,7 +7,7 @@
 import os
 import subprocess
 import sys
-import time
+from datetime import datetime
 
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -17,15 +17,17 @@ script_filename = os.path.basename(__file__)
 
 def print_test_info():
     print("This script tests the branch merge functionality of git.")
-    print("It works in eight steps:")
+    print("It works in ten steps:")
     print("1. (automatic) Create a new directory if it does not exist.")
     print("2. (automatic) Create a new Markdown file with some text in the directory.")
     print("3. (automatic) Commit the new file to the current branch.")
     print("4. (automatic) Create a new branch with a timestamp name.")
     print("5. (manual) Make some changes to the file in the new branch.")
     print("6. (manual) Commit the changes to the new branch.")
-    print("7. (manual) Merge the new branch into the main branch.")
-    print("8. (manual) Observe that the merge results are correct.")
+    print("7. (manual) Checkout back to the master branch.")
+    print("8. (manual) Make some changes to the file in the master branch.")
+    print("9. (manual) Merge the new branch into the main branch.")
+    print("10. (manual) Observe that the merge results are correct.")
 
 
 def create_directory_if_dne(directory_name):
@@ -34,19 +36,19 @@ def create_directory_if_dne(directory_name):
         print(f"Directory '{directory_name}' created successfully.")
 
 
-def check_if_file_exists():
-    # Check if the file exists, exiting if it does.
-    if os.path.exists(filename):
-        print(f"File '{filename}' already exists, proceeding.")
+def check_if_file_exists(arg_filename):
+    # Check if the file exsts, exiting if it does.
+    if os.path.exists(arg_filename):
+        print(f"File '{arg_filename}' already exists, proceeding.")
     else:
         # Proceed with the test step two and create the file.
         create_new_Markdown_file()
         # commit_file_to_git()
 
 
-def create_new_Markdown_file():
+def create_new_Markdown_file(input_directory):
     # Proceed with the test and create the file.
-    with open(filename, "w") as file:
+    with open(os.path.join(input_directory, filename), "w") as file:
         file.write("\n### Git Testing\n")
         file.write(f"\nThis file was created by script '{script_filename}'\n")
         file.write("The next few paragraphs are in numerical order.\n")
@@ -55,9 +57,9 @@ def create_new_Markdown_file():
     print(f"Markdown file '{filename}' created successfully.")
 
 
-def commit_file_to_git():
+def commit_file_to_git(arg_filename):
     commit_message = "b.m.t. auto-commit"
-    subprocess.run(["git", "add", filename])
+    subprocess.run(["git", "add", arg_filename])
     subprocess.run(["git", "commit", "-m", commit_message])
     print("File committed to git successfully.")
 
@@ -73,7 +75,7 @@ def git_status_is_clean() -> bool:
 
 def create_new_git_branch():
     # Create a new branch with a timestamp name.
-    branch_name = "branch-" + str(int(time.time()))
+    branch_name = "branch-" + datetime.now().strftime("%Y%m%d%H%M%S")
     subprocess.run(["git", "checkout", "-b", branch_name])
     print(f"New branch '{branch_name}' created successfully.")
 
@@ -90,13 +92,14 @@ if __name__ == "__main__":
         print("Please provide a directory name as a command line argument.")
         print("Or input '--help' for more information.")
     else:
-        if sys.argv[1] == "--help":
+        if sys.argv[1] == "--help" or sys.argv[1] == "-h":
             print_test_info()
             sys.exit()
         else:
-            directory_name = sys.argv[1]
-            create_directory_if_dne(directory_name)
-            check_if_file_exists()
-            create_new_Markdown_file()
-            commit_file_to_git()
+            input_directory = sys.argv[1]
+            new_filename = os.path.join(input_directory, filename)
+            create_directory_if_dne(input_directory)
+            check_if_file_exists(new_filename)
+            create_new_Markdown_file(new_filename)
+            commit_file_to_git(new_filename)
             create_new_git_branch()
